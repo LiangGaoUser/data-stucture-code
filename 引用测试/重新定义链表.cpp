d78830374b208f6386d69112a2c08b2f06c9ad7f
LiangGaoUser <1920746538@qq.com>
2019-03-02 23:02:31 +0800

    添加项目文件。

---
+++ 引用测试/重新定义链表.cpp
@@ -0,0 +1,19 @@
+#include "malloc.h"
+#define Status int
+#define ElemType int
+typedef struct LNode
+{
+	ElemType data;
+	struct LNode *next;
+}*Link,*Position;
+
+typedef struct
+{
+	Link head, tail;//定义指向链表的头结点和最后一个结点的
+	int len;//定义链表的长度，在单链表中则没有
+}LinkList;
+Status InitList(LinkList&L)//构造带有头结点的空表
+{
+	L = (LinkList)malloc(sizeof(LinkList));
+
+}
